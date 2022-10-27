82a45a82517844b959c7578286b574fc4ab22e96
kldufour <kellydufour97@gmail.com>
2022-10-27 09:36:14 +0200

    feat: Hello world

--- src/web/service/bike/BikeServer.java
+++ src/web/service/bike/BikeServer.java
@@ -3,7 +3,7 @@ package web.service.bike;
 public class BikeServer {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello World");
     }
 
 }
