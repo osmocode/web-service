68283afbd15b8b5d191521af83ef4d417eccb36c
Jason.D <jason.decilap@gmail.com>
2022-11-26 15:19:32 +0100

    Bug: Bike Service

--- rmi-bike/src/main/java/web/service/rmi/bike/models/bike/BikeList.java
+++ rmi-bike/src/main/java/web/service/rmi/bike/models/bike/BikeList.java
@@ -1,12 +1,12 @@
 package web.service.rmi.bike.models.bike;
 
+import web.service.rmi.bike.interfaces.bike.BikeInterface;
 import web.service.rmi.bike.interfaces.bike.BikeListInterface;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -24,13 +24,13 @@ public class BikeList extends UnicastRemoteObject implements BikeListInterface {
     }
 
     @Override
-    public Optional<List<Bike>> getBikeByOwner(long id) throws RemoteException {
+    public Optional<List<BikeInterface>> getBikeByOwner(long id) throws RemoteException {
         // TODO
         return Optional.ofNullable(null);
     }
 
     @Override
-    public List<Bike> getBikesList() {
+    public List<BikeInterface> getBikesList() {
         return bikeMap.values().stream().collect(Collectors.toList());
     }
 }
