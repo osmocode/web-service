7e14cc2f470c164b4bf7b9b2d635c6b5674a6ad5
osmocode <96887741+osmocode@users.noreply.github.com>
2022-12-03 20:00:16 +0100

    faet: init bank web-service

--- web-service-bank/src/main/java/web/service/bank/Config.java
+++ web-service-bank/src/main/java/web/service/bank/Config.java
@@ -1,4 +1,4 @@
-package web.service.convertor;
+package web.service.bank;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
-import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -14,21 +13,21 @@ import org.springframework.xml.xsd.XsdSchema;
 
 @EnableWs
 @Configuration
-public class Config extends WsConfigurerAdapter {
+public class Config {
 
     @Bean
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean(servlet, "/ws/convertor/*");
+        return new ServletRegistrationBean(servlet, "/ws/bank/*");
     }
 
-    @Bean(name = "convertor")
+    @Bean(name = "bank")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema bikeSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-        wsdl11Definition.setPortTypeName("ConvertorPort");
-        wsdl11Definition.setLocationUri("/ws/convertor/convertor");
+        wsdl11Definition.setPortTypeName("BankPort");
+        wsdl11Definition.setLocationUri("/ws/bank/bank");
         wsdl11Definition.setTargetNamespace("http://www.springframework.org/schema/web-services");
         wsdl11Definition.setSchema(bikeSchema);
         return wsdl11Definition;
@@ -36,15 +35,7 @@ public class Config extends WsConfigurerAdapter {
 
     @Bean
     public XsdSchema bikeSchema() {
-        return new SimpleXsdSchema(new ClassPathResource("convertor.xsd"));
-    }
-
-    @Bean
-    public Convertor getService() {
-        var service = new Convertor();
-        service.addCurrency(new Convertor.Currency("USD", "EURO", 0.5));
-        service.addCurrency(new Convertor.Currency("EURO", "USD", 2.0));
-        return service;
+        return new SimpleXsdSchema(new ClassPathResource("bank.xsd"));
     }
 
 }
