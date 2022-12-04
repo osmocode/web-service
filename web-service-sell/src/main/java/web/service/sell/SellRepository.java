7db563d8a1b6faef1176adef1fce1d6ceb20d906
Jason.D <jason.decilap@gmail.com>
2022-12-04 23:39:46 +0100

    Fix: not same bike in basket

--- web-service-sell/src/main/java/web/service/sell/SellRepository.java
+++ web-service-sell/src/main/java/web/service/sell/SellRepository.java
@@ -123,10 +123,11 @@ public class SellRepository {
         Objects.requireNonNull(uuid);
 
         if (baskets.get(uuid) == null) {
-            return ;
+            return;
         }
 
-        baskets.get(uuid).removeAll(baskets.get(uuid).stream().distinct().filter(this::isSold).toList());
+
+        baskets.put(uuid, baskets.get(uuid).stream().distinct().filter(s -> !isSold(s)).toList());
     }
 
     public List<Article> getBasket(GetBasketRequest request) throws RemoteException {
@@ -175,7 +176,7 @@ public class SellRepository {
             return null;
         }
 
-        if (article.getOwner().equals(authService.isLogged(UUID.fromString(request.getToken())).toString())) {
+        if (article.getOwner().equals(customerUuid)) {
             return null;
         }
 
