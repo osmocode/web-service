8d276839a1cb78bb29aca2a9278566e5de24cbde
HayawanZigou <hassanimehdi19955@gmail.com>
2022-12-04 18:08:07 +0100

    Feature: Added endpoint customer/{id}/feedback

--- rest-api/src/main/java/web/service/rest/controllers/CustomerController.java
+++ rest-api/src/main/java/web/service/rest/controllers/CustomerController.java
@@ -4,20 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import rmi.bike.interfaces.feedback.FeedbackListService;
 import rmi.customer.interfaces.CustomerListService;
 import rmi.customer.models.CustomerType;
 import web.service.rest.providers.CustomerListProvider;
 import web.service.rest.providers.CustomerProvider;
+import web.service.rest.providers.FeedbackListProvider;
 
 import javax.validation.Valid;
 import java.rmi.RemoteException;
 import java.util.UUID;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 @RestController
 public class CustomerController {
 
     @Autowired
     private CustomerListService service;
+
+    @Autowired
+    private FeedbackListService feedbackService;
 
     @GetMapping("/api/v1/customer")
     public CustomerListProvider getCustomerAll() throws RemoteException {
@@ -31,6 +39,25 @@ public class CustomerController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found");
         }
         return new CustomerProvider(UUID.fromString(uuid), customer);
+    }
+
+    @GetMapping("/api/v1/customer/{id}/feedback")
+    public FeedbackListProvider getCustomerFeedbackById(@PathVariable("id") String uuid) throws RemoteException {
+        var customer = service.getCustomerByUUID(uuid);
+        if (customer == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found");
+        }
+        var res = feedbackService.getAll().entrySet().stream()
+        .filter(t -> {
+            try {
+                return t.getValue().getRent().getCustomerClientUUID().equals(UUID.fromString(uuid));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                return false;
+            }
+        })
+        .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+        return new FeedbackListProvider(res);
     }
 
     @PostMapping("/api/v1/customer")
